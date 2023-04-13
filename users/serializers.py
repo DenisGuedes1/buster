61167a824eaef388c7bb4b9841b7e75c46613cf8
Denis <denisguedesmoraes@gmail.com>
2023-04-12 21:13:49 -0300

    task 1 finally

---
+++ users/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import User
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=127)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    password = serializers.CharField(write_only=True)
+    birthdate = serializers.DateField(required=False)
+    is_employee = serializers.BooleanField(required=False, default=False)
+    is_superuser = serializers.BooleanField(read_only=True, source='user.is_superuser')
+
+    def validate(self, data):
+        email = data.get('email', None)
+        username = data.get('username', None)
+
+        error={}
+
+        if email and User.objects.filter(email=email):
+            error['email'] = ['email already registered']
+
+        if username and User.objects.filter(username=username):
+           error['username'] = ['Username already taken']
+
+        if error:
+            raise serializers.ValidationError(error)       
+
+        return data
+
+    def create(self, validated_data):
+        is_employee = validated_data.pop('is_employee', False)
+        user = User.objects.create_user(**validated_data)
+        user.is_employee = is_employee
+        if is_employee:
+            user.is_superuser = is_employee
+            print("bopllenos" ,user.is_superuser)
+            user.is_employee
+         
+            user.save()
+        return user
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        if instance.is_employee:
+            rep['is_superuser'] = True
+        return rep
---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import User
+
+
+
+urlpatterns = [
+    path('users/', User.as_view())
+]
